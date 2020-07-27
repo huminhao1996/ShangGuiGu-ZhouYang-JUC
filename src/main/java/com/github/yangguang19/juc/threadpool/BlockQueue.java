7c73347605f945dfa709d6c22be7e17b87784af6
hmh <454977355@qq.com>
2020-07-27 20:23:55 +0800

    1.异步回调的例子

--- src/main/java/com/github/yangguang19/juc/threadpool/BlockQueue.java
+++ src/main/java/com/github/yangguang19/juc/threadpool/BlockQueue.java
@@ -11,7 +11,40 @@ public class BlockQueue {
     public static void main(String[] args) throws Exception {
         BlockingQueue<String> arrayBlockQueue = new ArrayBlockingQueue<>(3);
 
-        //这一组不会一直阻塞等待,而是阻塞一段时间
+        /** 抛出异常 组*/
+//        System.out.println(arrayBlockQueue.add("a"));
+//        System.out.println(arrayBlockQueue.add("b"));
+//        System.out.println(arrayBlockQueue.add("c"));
+//        System.out.println(arrayBlockQueue.add("d"));   //超出队列界限,抛出异常
+//        System.out.println(arrayBlockQueue.remove());
+//        System.out.println(arrayBlockQueue.remove());
+//        System.out.println(arrayBlockQueue.remove());
+//        System.out.println(arrayBlockQueue.remove());   //队列中没有元素,将抛出异常
+//        System.out.println(arrayBlockQueue.element());  //队列中没有元素,检查时也会抛出异常
+
+          /**这一组不会抛出异常,而是返回false,null值 */
+//        System.out.println(arrayBlockQueue.offer("a"));
+//        System.out.println(arrayBlockQueue.offer("b"));
+//        System.out.println(arrayBlockQueue.offer("c"));
+//        System.out.println(arrayBlockQueue.offer("d")); //返回false
+//
+//        System.out.println(arrayBlockQueue.poll());
+//        System.out.println(arrayBlockQueue.poll());
+//        System.out.println(arrayBlockQueue.poll());
+//        System.out.println(arrayBlockQueue.poll());     //返回null
+
+          /**这一组不会返回特殊值, 而是阻塞等待, 也就是适用于线程池的消费者和生产者 */
+//        arrayBlockQueue.put("a");
+//        arrayBlockQueue.put("b");
+//        arrayBlockQueue.put("c");
+////        arrayBlockQueue.put("d");   //main线程阻塞
+//
+//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take());
+//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take());
+//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take());
+//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take()); //main线程阻塞
+
+        /** 这一组不会一直阻塞等待,而是阻塞一段时间,超时则退出*/
         System.out.println(arrayBlockQueue.offer("a", 3L, TimeUnit.SECONDS));
         System.out.println(arrayBlockQueue.offer("b", 3L, TimeUnit.SECONDS));
         System.out.println(arrayBlockQueue.offer("c", 3L, TimeUnit.SECONDS));
@@ -23,39 +56,6 @@ public class BlockQueue {
         System.out.println(arrayBlockQueue.poll(3L, TimeUnit.SECONDS));
         //等待3秒,还没有元素可以poll,就返回null
         System.out.println("等待3秒了,线程还是在阻塞: " + arrayBlockQueue.poll(3L, TimeUnit.SECONDS));
-
-//        /** 抛出异常 组*/
-//        System.out.println(arrayBlockQueue.add("a"));
-//        System.out.println(arrayBlockQueue.add("b"));
-//        System.out.println(arrayBlockQueue.add("c"));
-//        System.out.println(arrayBlockQueue.add("d"));   //超出队列界限,抛出异常
-//        System.out.println(arrayBlockQueue.remove());
-//        System.out.println(arrayBlockQueue.remove());
-//        System.out.println(arrayBlockQueue.remove());
-//        System.out.println(arrayBlockQueue.remove());   //队列中没有元素,将抛出异常
-//        System.out.println(arrayBlockQueue.element());  //队列中没有元素,检查时也会抛出异常
-
-        //        //这一组不会抛出异常,而是返回false,null值
-//        System.out.println(arrayBlockQueue.offer("a"));
-//        System.out.println(arrayBlockQueue.offer("b"));
-//        System.out.println(arrayBlockQueue.offer("c"));
-//        System.out.println(arrayBlockQueue.offer("d")); //返回false
-//
-//        System.out.println(arrayBlockQueue.poll());
-//        System.out.println(arrayBlockQueue.poll());
-//        System.out.println(arrayBlockQueue.poll());
-//        System.out.println(arrayBlockQueue.poll());     //返回null
-
-        //这一组不会返回特殊值, 而是阻塞等待, 也就是适用于线程池的消费者和生产者
-//        arrayBlockQueue.put("a");
-//        arrayBlockQueue.put("b");
-//        arrayBlockQueue.put("c");
-////        arrayBlockQueue.put("d");   //main线程阻塞
-//
-//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take());
-//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take());
-//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take());
-//        System.out.println("从阻塞队列中拿走了: " + arrayBlockQueue.take()); //main线程阻塞
     }
 
 }
